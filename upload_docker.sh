434dd28bbed7874cc738cafcfb7b60c8ede9a3a7
Mohit Bansal <mohitbansal.it@gmail.com>
2020-09-06 12:04:55 +0000

    Project 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="mohit062000/mludacityapi"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username mohit062000
+    docker image tag mludacityapi $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
